ed5dc6bc6d40d1a3e9bde406f3d9adf62bf174b0
Yunfei Hao <yunfei.hao@intel.com>
2017-03-30 11:16:33 +0800

    Version 0.0.1

--- getDistance.js
+++ getDistance.js
@@ -5,6 +5,8 @@
 'use strict';
 
 let pt = require('person-tracking');
+var childProcess = require('child_process');
+var colors = require('colors');
 let trackerOptions = {
   skeleton: {
     enable: true,
@@ -44,19 +46,107 @@ let cameraOptions = {
 };
 
 let tracker = null;
-let recogCtl = null;
 let registerOngoing = false;
 let registerPerformed = false;
 let intervalId = null;
+let cmd_curl = 'curl http://192.168.12.230:8000/api/oic/res';
+let lastModifyTime;
+let ledList = {};
+let led1LastChangeTime;
+let led2LastChangeTime;
+let led3LastChangeTime;
+let led4LastChangeTime;
+function initialLED() {
+    childProcess.exec(cmd_curl, function(err,stdout,stderr){
+        if(err) {
+            console.log('Failed to get led id with error:'+stderr);
+            process.exit();
+        } else {
+            var data = JSON.parse(stdout);
+            data.forEach(function (item){
+               if(item.links[0].href.indexOf('/a/led') > -1){
+                   ledList[item.links[0].href] = item.di;  
+               }
+           })
+        }
+    });
+}
+console.log(new Date);
+initialLED();
+console.log(new Date);
+let led1FlgOld = false;
+let led2FlgOld = false;
+let led3FlgOld = false;
+let led4FlgOld = false;
+function controlLEDbyPersons(persons) {
+  let led1Flg = false;
+  let led2Flg = false;
+  let led3Flg = false;
+  let led4Flg = false;
+  persons.forEach((person) => {
+    let distance = 0;
+    if(person.trackInfo && person.trackInfo.center){
+      distance = person.trackInfo.center.worldCoordinate.z;
+    }
+    if(distance <= 1 && distance > 0) {
+      console.log(colors.blue(distance));
+      led1Flg = true;
+    }
+    if(distance <= 1.3 && distance > 1) {
+      console.log(colors.red(distance));
+      led2Flg = true;
+    }
+    if(distance <= 1.6 && distance > 1.3) {
+      console.log(colors.white(distance));
+      led3Flg = true;
+    }
+    if(distance <= 1.9 && distance > 1.6) {
+      console.log(colors.green(distance));
+      led4Flg = true;
+    }
+  });
+  if(led1FlgOld !== led1Flg) {
+    if (led1Flg){
+      console.log(('update led 1 with' + led1Flg).blue.bold);
+    } else {
+      console.log(('update led 1 with' + led1Flg).blue.inverse);
+    }
+    updateLedStatus(2, '/a/led2', led1Flg); led1FlgOld = led1Flg;
+  }
+  if(led2FlgOld !== led2Flg) {
+    if (led2Flg){
+      console.log(('update led 2 with' + led2Flg).red.bold);
+    } else {
+      console.log(('update led 2 with' + led2Flg).red.inverse);
+    }
+    updateLedStatus(12, '/a/led12', led2Flg); led2FlgOld = led2Flg;
+  } 
+  if(led3FlgOld !== led3Flg) {
+    if (led3Flg){
+      console.log(('update led 3 with' + led3Flg).white.bold);
+    } else {
+      console.log(('update led 3 with' + led3Flg).white.inverse);
+    }
+    updateLedStatus(7, '/a/led7', led3Flg); led3FlgOld = led3Flg;
+  }
+  if(led4FlgOld !== led4Flg) {
+    if (led4Flg){
+      console.log(('update led 4 with' + led4Flg).green.bold);
+    } else {
+      console.log(('update led 4 with' + led4Flg).green.inverse);
+    }
+    updateLedStatus(9, '/a/led9', led4Flg); led4FlgOld = led4Flg;
+  }
+}
+
 pt.createPersonTracker(trackerOptions, cameraOptions).then((personTracker) => {
   tracker = personTracker;
-  recogCtl = tracker.faceRecognition;
-  intervalId = setInterval(function() {
-    console.log('------------------ state: ', tracker.state);
-  }, 500);
-
-  operate('start', 5000, 'Start');
-  operate('stop', 15000, 'Stop');
+  tracker.on('persontracked', (result) => {
+    controlLEDbyPersons(result.persons);
+  });
+  return tracker.start();
+  //operate('start', 5000, 'Start');
+  //operate('stop', 150000, 'Stop');
 }).catch((e) => {
   console.log('Failed to start, ', e);
   clearInterval(intervalId);
@@ -65,143 +155,39 @@ pt.createPersonTracker(trackerOptions, cameraOptions).then((personTracker) => {
 function registerTestWork() {
   tracker.on('persontracked', function(result) {
     result.persons.forEach(function(person) {
-      // skeletonTest(person);
       trackingTest(person);
-      // personFaceTest(person);
-      // gestureTest(person);
-      // Comment register test as it's not formally supported yet.
-      // registerAndReinforceTest(person);
     });
   });
 }
-// eslint-disable-next-line
-function registerAndReinforceTest(person) {
-  console.log('-- registration result begin');
-  let ids = null;
-  if (person.trackInfo) {
-    if (registerPerformed === false) {
-      if (registerOngoing === false) {
-        registerOngoing = true;
-        recogCtl.registerPerson(person.trackInfo.id).then(function(recogID) {
-          console.log('--register person success track ID, recog ID:',
-              person.trackInfo.id, recogID);
-          registerPerformed = true;
-          return recogCtl.reinforceRegistration(person.trackInfo.id, recogID, 'manual-add');
-        }).then(function() {
-          console.log('--reinforce person success');
-        }).then(function() {
-          return recogCtl.getRegisteredIDs();
-        }).then(function(theIds) {
-          console.log('--registered IDS:', theIds);
-          ids = theIds;
-          return recogCtl.isPersonRegistered(ids[0]);
-        }).then(function(exist) {
-          console.log('--ids[0] registered? :', exist);
-          console.log('--unRegister ids[0]:', ids[0]);
-          return recogCtl.unRegisterPerson(ids[0]);
-        }).then(function() {
-          console.log('--ids[0] unregistered:', ids[0]);
-        }).then(function() {
-          return recogCtl.getRegisteredIDs();
-        }).then(function(newids) {
-          console.log('--final registered ids: ', newids);
-          registerOngoing = false;
-        }).catch(function(err) {
-          console.log('--register person test failed, track ID:' + person.trackInfo.id +
-              ' failed: ' + err);
-          registerOngoing = false;
-        });
-      }
-    }
-  }
-  console.log('-- registration result end');
-}
-
-function skeletonTest(person) {
-  console.log('-- skeleton result begin');
-  if (person.skeletonInfo) {
-    console.log('-- skeleton:');
-    person.skeletonInfo.skeletonJoints.forEach(function(point) {
-      console.log(point);
-    });
-  }
-  console.log('-- skeleton result end');
-}
-
-function gestureTest(person) {
-  console.log('-- gesture result begin');
-  let gesture = person.gestureInfo;
-  if (!gesture)
-    return;
-  let info = gesture.pointingInfo;
-  if (gesture.isPointing && info) {
-    console.log('--gesture: pointingInfo: colorOrigin(x,y), direction(x,y), confidence,' +
-        'timestamp: ', info.colorPointingData.origin.x, info.colorPointingData.origin.y,
-        info.colorPointingData.direction.x, info.colorPointingData.direction.y,
-        info.confidence, info.gestureStartTimeStamp);
-    console.log('--gesture: pointingInfo: worldOrigin(x,y,z), direction(x,y,z)',
-        info.worldPointingData.origin.x, info.worldPointingData.origin.y,
-        info.worldPointingData.origin.z, info.worldPointingData.direction.x,
-        info.worldPointingData.direction.y, info.worldPointingData.direction.z);
-  }
-  console.log('-- gesture result end');
-}
-
-function personFaceTest(person) {
-  console.log('-- personFace result begin');
-  let face = person.faceInfo;
-  if (!face)
-    return;
-  let pose = face.headPose;
-  let markInfo = face.landmarkInfo;
-
-  if (pose) {
-    console.log('--head pose: yaw, pitch, roll:', pose.yaw, pose.pitch, pose.roll);
-  }
-  if (markInfo) {
-    let mark = markInfo.landmarks[0];
-    console.log('--landmark: confidence:' + markInfo.confidence + ' markcount: ' +
-        markInfo.landmarks.length);
-    console.log('--mark[0]:' + mark.imageCoordinate.x + ',' + mark.imageCoordinate.y);
-  }
-  console.log('-- personFace result end');
-}
 
 function trackingTest(person) {
-  console.log('-- tracking result begin');
+  // console.log('-- tracking result begin');
   let info = person.trackInfo;
   if (!info)
     return;
-  let box = info.boundingBox;
-  let hbox = info.headBoundingBox;
   let center = info.center;
   let seg = info.segmentationMask;
 
   tracker.personTracking.startTrackingPerson(person.trackInfo.id).then(function() {
-    console.log('-- successfully started tracking person');
+    //console.log('-- successfully started tracking person');
   }).catch(function(e) {
     console.log('-- failed tracking person');
   });
 
   console.log('--id:' + info.id);
-  if (box) {
-    console.log('--box: confidence: ' + box.confidence + ', x: ' + box.rect.x +
-        ', y: ' + box.rect.y + ', w: ' + box.rect.width + ', h: ' + box.rect.height);
-  }
-  if (hbox) {
-    console.log('--headbox: confidence: ' + hbox.confidence + ', x: ' + hbox.rect.x + ', y: ' +
-        hbox.rect.y + ', w: ' + hbox.rect.width + ', h: ' + hbox.rect.height);
-  }
   if (center) {
-    console.log('--center: imageCoordinate and confidence:', center.imageCoordinate.x,
-        center.imageCoordinate.y, center.imageConfidence);
-    console.log('--center: worldCoordinate and confidence:', center.worldCoordinate.x,
-        center.worldCoordinate.y, center.worldCoordinate.z, center.worldConfidence);
+    //console.log('--center: imageCoordinate and confidence:', center.imageCoordinate.x,
+    //    center.imageCoordinate.y, center.imageConfidence);
+    //console.log('--center: worldCoordinate and confidence:', center.worldCoordinate.x,
+    //    center.worldCoordinate.y, center.worldCoordinate.z, center.worldConfidence);
+    if(center.worldCoordinate.z) {
+      controlLEDbyDistance(center.worldCoordinate.z);
+    }
   }
   if (seg) {
     console.log('--segmentation: width/height', seg.width, seg.height, seg.maskData);
   }
-  console.log('-- tracking result end');
+  //console.log('-- tracking result end');
 }
 
 function operate(op, time, des) {
@@ -224,3 +210,57 @@ function operate(op, time, des) {
     });
   }, time + 500);
 }
+
+//function getLedId(ledNum, state) {
+//    childProcess.exec(cmd_curl, function(err,stdout,stderr){
+//        if(err) {
+//            console.log('Failed to get led id with error:'+stderr);
+//        } else {
+//            var data = JSON.parse(stdout);
+//            data.forEach(function (item){
+//               var target_href = ['/a/led', ledNum].join('')
+//               //console.log(target_href)
+//               if(item.links[0].href == '/a/led'){
+//                   console.log(item.links[0].href)
+//                   console.log(item.di)
+//                   var url = [item.links[0].href, '?di=', item.di].join('')
+//                   updateLedStatus(ledNum, url, state)
+//               }
+//           })
+//        }
+//    });
+//}
+
+
+function updateLedStatus(ledNum, url, state) {
+    if(ledNum === 1 && new Date - led1LastChangeTime <= 1000) {
+      return; 
+    }
+    if(ledNum === 2 && new Date - led2LastChangeTime <= 1000) {
+      return; 
+    }
+    if(ledNum === 3 && new Date - led3LastChangeTime <= 1000) {
+      return; 
+    }
+    if(ledNum === 4 && new Date - led4LastChangeTime <= 1000) {
+      return; 
+    }
+    let serverFile;
+    if(state) {
+        serverFile = './test/led-on.json'
+    } else {
+        serverFile = './test/led-off.json'
+    }
+    url = [url, '?di=', ledList[url]].join('');
+//console.log(url);
+    //childProcess.execFile('./test/oic-post', [url, serverFile], function(err, stdout, stderr){
+console.log("./test/oic-put "+ url + " " + serverFile);
+    childProcess.execFile('./test/oic-put', [url, serverFile], function(err, stdout, stderr){
+        if(err) {
+            console.log('Failed to update led status with error: '+stderr);
+        } else {
+           // console.log('Turn', state, ['led', ledNum].join(''))
+        }
+    })
+}
+
